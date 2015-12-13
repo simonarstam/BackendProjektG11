165a68e362e152ef521cbb4f6c517f67b2da85f7
simonarstam <sarstam@kth.se>
2015-12-13 18:52:53 +0100

    Test
    sd

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test");
+            WfcService1.Service1Client client = new WfcService1.Service1Client();
+            Console.WriteLine(client.GetData(4));
+            client.Close();
+            Console.ReadKey();
+        }
+    }
+}
